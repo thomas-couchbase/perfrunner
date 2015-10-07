7003e2e01951373663dbcfda8be2d19f25ad3a2c
Thomas Anderson <thomas@couchbase.com>
2015-10-07 12:02:32 -0700

    rebalance test additions

--- perfrunner/helpers/worker.py
+++ perfrunner/helpers/worker.py
@@ -1,3 +1,4 @@
+import pdb
 import sys
 from time import sleep
 
@@ -115,6 +116,7 @@ class RemoteWorkerManager(object):
                      run_workload=task_run_workload):
         self.workers = []
         for target in target_iterator:
+            pdb.set_trace()
             log_phase('workload generator', settings)
 
             qname = '{}-{}'.format(target.node.split(':')[0], target.bucket)
